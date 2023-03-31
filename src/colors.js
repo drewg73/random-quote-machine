f1f68c0c3409e3e38dad6d1b843de11bfd3576ae
Drew Garbenis <70610167+drewg73@users.noreply.github.com>
2023-03-30 19:39:03 -0700

    commit

---
+++ src/colors.js
@@ -0,0 +1,11 @@
+const colors = [
+    "#FFADAD",
+    "#FFD6A5",
+    "#CAFFBF",
+    "#9BF6FF",
+    "#A0C4FF",
+    "#BDB2FF",
+    "#FFC6FF",
+];
+
+export default colors;
--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -1,38 +1,54 @@
 import React, { useState } from 'react'
 import quotes from '../quotes';
+import colors from '../colors';
 import "./QuoteBox.css";
 
 let initialQuote = quotes[Math.floor(Math.random() * quotes.length - 1)];
+let initialColor = colors[Math.floor(Math.random() * colors.length - 1)];
 
 export default function QuoteBox() {
   const [quote, setQuote] = useState(initialQuote);
+  const [color, setColor] = useState(initialColor);
+
+  const cssColor = {
+    color: `${color}`,
+  };
+  const cssBackgroundColor = {
+    backgroundColor: `${color}`,
+  };
   
   function handleClick() {
     let filteredQuotes = quotes.filter(q => q !== quote);
     let randomQuote = filteredQuotes[Math.floor(Math.random() * (filteredQuotes.length - 1))];
     setQuote(randomQuote);
+    let filteredColors = colors.filter(c => c !== color);
+    let randomColor = filteredColors[Math.floor(Math.random() * (filteredColors.length - 1))];
+    setColor(randomColor);
   }
 
   return (
-    <div id="quote-box">
-        <p id="text"><span>"</span> {quote.quote}</p>
-        <p id="author">- {quote.author}</p>
-        <div>
-            <button>
-                <a  
-                    id="tweet-quote"
-                    href="twitter.com/intent/tweet"
-                >
-                    Tweet Quote
-                </a>
-            </button>
-            <button
-                id="new-quote"
-                onClick={handleClick}
-            >
-                New Quote
-            </button>
-        </div>
+    <div class="wrapper" style={cssBackgroundColor}>
+      <div id="quote-box">
+          <p id="text" style={cssColor}><span>"</span> {quote.quote}</p>
+          <p id="author" style={cssColor}>- {quote.author}</p>
+          <div>
+              <button style={cssBackgroundColor}>
+                  <a  
+                      id="tweet-quote"
+                      href="twitter.com/intent/tweet"
+                  >
+                      Tweet Quote
+                  </a>
+              </button>
+              <button
+                  id="new-quote"
+                  onClick={handleClick}
+                  style={cssBackgroundColor}
+              >
+                  New Quote
+              </button>
+          </div>
+      </div>
     </div>
   )
 }
